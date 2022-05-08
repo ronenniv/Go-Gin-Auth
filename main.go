709cfe58350b1d6a589c357020304068c43751bb
ronenniv <46080320+ronenniv@users.noreply.github.com>
2022-05-08 02:20:03 -0400

    add put recipe

--- main.go
+++ main.go
@@ -17,23 +17,59 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+type Message struct {
+	Message string `json:"error"`
+}
+
 var recipes []Recipe
 
 func NewRecipeHandler(c *gin.Context) {
 	var recipe Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, Message{Message: err.Error()})
 		return
 	}
 	recipe.ID = xid.New().String()
 	recipe.PublishedAt = time.Now()
 	recipes = append(recipes, recipe)
 	c.JSON(http.StatusOK, recipe)
+}
 
+func ListRecipesHandler(c *gin.Context) {
+	if recipes == nil {
+		c.JSON(http.StatusNotFound, Message{Message: "No recipes"})
+		return
+	}
+	c.JSON(http.StatusOK, recipes)
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, Message{Message: err.Error()})
+		return
+	}
+	recipe.ID = id
+	found := false
+	for i, r := range recipes {
+		if r.ID == recipe.ID {
+			recipes[i] = recipe
+			found = true
+			break
+		}
+	}
+	if found {
+		c.JSON(http.StatusOK, recipe)
+	} else {
+		c.JSON(http.StatusNotFound, Message{Message: id + " not found"})
+	}
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
 }
